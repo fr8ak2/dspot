7d33e9fea4b5cfaf68b7eab387fb015e3bcd6bbb
Sergey <info@rou.digital>
2024-08-20 01:13:59 +0300

    Back and Close lightbox buttons

--- src/components/Friends/Info/FriendsInfo.tsx
+++ src/components/Friends/Info/FriendsInfo.tsx
@@ -1,5 +1,6 @@
 import { clsx } from 'clsx'
 import Image from 'next/image'
+import Link from 'next/link'
 import { FC } from 'react'
 
 import { Photos } from '@/mock/photos'
@@ -7,6 +8,7 @@ import { Available, Box, Status } from '@/shared'
 import { Lightbox } from '@/shared/Lightbox/Lightbox'
 import { Tabs } from '@/shared/Tabs/Tabs'
 import { FriendDetail } from '@/types/friends'
+import { FRIENDS_ENDPOINT } from '@/utils'
 
 import styles from './FriendsInfo.module.scss'
 
@@ -27,6 +29,23 @@ const FriendsInfo: FC<FriendInfoProps> = ({ friend }) => {
     return (
         <Box className={styles.module}>
             <Box className={styles.inner}>
+                <Box className={styles.back}>
+                    <Link href="/">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="16"
+                            height="14"
+                            fill="none"
+                            viewBox="0 0 16 14"
+                        >
+                            <path
+                                fill="#2E57FA"
+                                d="M15 6H3.14l3.63-4.36A1.001 1.001 0 0 0 5.23.36l-5 6a1.19 1.19 0 0 0-.09.15c0 .05 0 .08-.07.13A1 1 0 0 0 0 7a1 1 0 0 0 .07.36c0 .05 0 .08.07.13.026.052.056.102.09.15l5 6A1 1 0 0 0 6 14a1 1 0 0 0 .77-1.64L3.14 8H15a1 1 0 1 0 0-2Z"
+                            />
+                        </svg>
+                    </Link>
+                </Box>
+
                 <Box className={styles.header}>
                     <Box className={styles.img}>
                         <Available available={friend.available} size="medium" />
